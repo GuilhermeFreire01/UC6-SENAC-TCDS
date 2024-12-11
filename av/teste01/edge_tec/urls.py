57c3a13875413a42e15f52dfb82ab41fde0afa16
Aluno <aluno@alunos.ce.senac.br>
2024-12-11 20:45:59 -0300

    aula 11/12/24

--- av/teste01/edge_tec/urls.py
+++ av/teste01/edge_tec/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-
+    path('contatos', views.contatos, name='contatos'),
+    path('sobre', views.sobre, name='sobre'),
+    path('blog', views.blog, name='blog'),
+    path('enderecos', views.enderecos, name='enderecos'),
+    path('parceiros', views.parceiros, name='parceiros'),
 ]
--- av/teste01/edge_tec/views.py
+++ av/teste01/edge_tec/views.py
@@ -4,5 +4,20 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home.html")
 
+def contatos(request):
+    return render (request, "contatos.html")
+
+def sobre(request):
+    return render(request, "sobre.html")
+
+def blog(request):
+    return render(request, "blog.html")
+
+def enderecos(request):
+    return render(request, "enderecos.html")
+
+def parceiros(request):
+    return render(request, "parceiros.html")
+
 
 # Create your views here.
